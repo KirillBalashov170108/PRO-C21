71cedfa0cf2e64e66349519efbfe2c237d00dfae
KirillBalashov170108 <82855500+KirillBalashov170108@users.noreply.github.com>
2021-07-06 22:33:06 -0700

    Update sketch.js

--- PRO-C21/sketch.js
+++ PRO-C21/sketch.js
@@ -30,7 +30,7 @@
 
 			Engine.run(engine);  
 		        }
-                        function draw() {
+                function draw() {
 		        rectMode(CENTER);
 		        background(0);
 
@@ -41,8 +41,8 @@
 			rightSide.display();
 			}
 
-		        function keyPressed() {
-			   if (keyCode === UP_ARROW) {
+	       function keyPressed() {
+			if (keyCode === UP_ARROW) {
 
 			Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
 			}
