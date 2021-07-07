5584a4f5cfdf16972ae605a185f8f7b156ec7db1
KirillBalashov170108 <82855500+KirillBalashov170108@users.noreply.github.com>
2021-07-06 22:29:11 -0700

    Update sketch.js

--- PRO-C21/sketch.js
+++ PRO-C21/sketch.js
@@ -29,21 +29,21 @@
 			rightSide = new ground(1300,600,20,120);
 
 			Engine.run(engine);  
-		}
-        function draw() {
-		rectMode(CENTER);
-		background(0);
+		        }
+                        function draw() {
+		        rectMode(CENTER);
+		        background(0);
 
-        ellipse(ball.position.x,ball.position.y,radius);
+                        ellipse(ball.position.x,ball.position.y,radius);
 
-		groundObj.display();
-		leftSide.display();  
-		rightSide.display();
-		}
+			groundObj.display();
+			leftSide.display();  
+			rightSide.display();
+			}
 
-		function keyPressed() {
-			if (keyCode === UP_ARROW) {
+		        function keyPressed() {
+			   if (keyCode === UP_ARROW) {
 
 			Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
 			}
-		}
+		      }
